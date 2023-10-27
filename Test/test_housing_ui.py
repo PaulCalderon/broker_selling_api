a89cf5de7f70950d9083abec1bee9b8e6dd22c63
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-10-27 18:00:39 +0800

    update test for housing ui

--- Test/test_housing_ui.py
+++ Test/test_housing_ui.py
@@ -1,15 +1,23 @@
 import pytest
 import os
-#from housing_ui import classmethod
+from housing_ui import validate_input
 
 class TestUIOfHousing:
     """Class for grouping tests"""
     def test_UI_should_validate_input_if_valid(self):
         """ To check if user submitted input is among valid commands"""
-        assert validate_input("valid")
+        assert validate_input("1")
+        assert validate_input("2")
+        assert validate_input("3")
+        assert validate_input("4")
+        assert validate_input("5")
+        assert validate_input("6")
     
     def test_UI_should_raise_error_if_input_is_invalid(self):
         """ To check if user submitted input is among valid commands"""
         with pytest.raises(ValueError):
             validate_input("invalid")
+
+        with pytest.raises(ValueError):
+            validate_input("7")
 
