3367431c58a00b42e4cf0b0ca7a8c2446375e764
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-10-27 18:01:08 +0800

    initial housing ui build

---
+++ housing_ui.py
@@ -0,0 +1,34 @@
+#acts as UI and application layer
+
+
+
+
+
+def validate_input(user_command: int) -> bool:
+    
+    match user_command:
+        case 1 | 2 | 3 | 4 | 5 | 6:
+            return True
+        case _:
+            raise ValueError("Invalid Command")
+
+def process_command(user_command):
+    match user_command:
+        case 1:
+            pass
+        case 2:
+            pass
+
+def help_command():
+    print("The following commands are available:")
+    print("1. Add House")
+    print("2. Update House")
+    print("3. Reserve House")
+    print("4. Check House Price")
+    print("5. Remove House")
+    print("6. Sell House")
+
+if __name__ == '__main__':
+    user_command = int(input("Enter the # Command:"))
+    validate_input(user_command)
+
