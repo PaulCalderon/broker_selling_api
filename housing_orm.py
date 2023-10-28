43124e408b7781a48357f7e425010a0dc4586987
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-10-28 18:32:34 +0800

    add housing orm add insert, update, delete, get functionality

--- housing_orm.py
+++ housing_orm.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from sqlalchemy.pool import NullPool
+from sqlalchemy.inspection import inspect
+
 DBFILE = "example.db"
 engine = create_engine("sqlite+pysqlite:///" + DBFILE , echo=False, poolclass=NullPool)
 
@@ -52,6 +54,10 @@ class DatabaseCommands():
         """dbname added for pytesting"""
         if dbname:
             database_name, engine = dbname
+        else:
+            database_name = DBFILE
+            engine = create_engine("sqlite+pysqlite:///" + DBFILE , echo=False, poolclass=NullPool)
+
 
         if not os.path.exists(database_name):
             table_initialize(database_name)
@@ -62,21 +68,67 @@ class DatabaseCommands():
 
 
     @staticmethod
-    def update() -> None:
-        pass
+    def update(table_class, table_object, engine_object, *updated_fields) -> None:
+        """takes updated table object and pushes to update database entry"""        """implementation is bad and just a workaround"""
+        primary_id_name = inspect(type(table_object)).primary_key[0].name
+        primary_id = getattr(table_object,primary_id_name)
+        #print(inspect(table_object).primary_key[0].name)
+
+
+            
+        with Session(engine_object) as session:
+            data = session.get(table_class, primary_id)
+            for fields in updated_fields:
+                setattr(data, fields, getattr(table_object,fields))
+            session.commit()
+
 
     @staticmethod
-    def delete() -> None:
-        pass
+    def delete(table_to_get_data_from, primary_id, *engine_object) -> None:
+        for data in engine_object:
+            engine = data
+        
+        with Session(engine) as session:
+            data = session.get(table_to_get_data_from, primary_id)
+            session.delete(data)
+            session.commit()
+
 
     @staticmethod
-    def get(table_to_get_data_from, house_id, *engine_object) -> HouseList:
+    def get(table_to_get_data_from, primary_id, *engine_object) -> HouseList:
+        """*arg is work around for pytest"""
         
         for data in engine_object:
             engine = data
 
         with Session(engine) as session:
-            data = session.get(table_to_get_data_from, house_id)
+            data = session.get(table_to_get_data_from, primary_id)
             return data
 
-        
+
+# if __name__ == "__main__":
+#     houses_object = HouseList(location_city="Makati City", developer = "SMDC", price = 50000)
+#     with Session(engine) as session:
+#         houses_object = HouseList(location_city="Makati City", developer = "SMDC", price = 50000)
+#         houses_object1 = session.get(HouseList,1)
+#         print(houses_object.developer)
+#         print(houses_object1.developer)
+#         houses_object1.developer = houses_object.developer
+
+
+#         #houses_object.developer = "RLC"
+#         session.flush()
+#         session.commit()
+#         print(houses_object.id_of_house)
+#         primary_id = inspect(type(houses_object)).primary_key[0].name
+#         print(primary_id)
+    # DatabaseCommands.update(houses_object)
+# if __name__ == "__main__":
+#     with Session(engine) as session:
+#         data = session.get(HouseList, 5)
+#         print(data)
+#         data.location_city = "Cavite"
+#         data.price = 90000
+#         DatabaseCommands.update(HouseList, data, engine, "price", "location_city")
+    
+#     pass
