7a285d4d378a9fce4b0766d5fc0e1812d952a704
PaulCalderon <117964753+PaulCalderon@users.noreply.github.com>
2023-11-02 21:35:16 +0800

    add more tests

--- Test/test_housing_application.py
+++ Test/test_housing_application.py
@@ -35,6 +35,35 @@ class TestAPIOfHousing:
         newly_inserted_price = DatabaseCommands.get(HouseList, house_id, engine)
         assert new_price == newly_inserted_price.price
 
+    def test_API_should_check_if_house_id_to_be_updated_already_exists_in_the_database(self, setup_database):
+        DBFILE, engine = setup_database
+        house_id = 100 #house id doesn't exist
+        new_price = 1000
+        with pytest.raises(ValueError, match="House id doesn't exist"):
+            HousingAPI.update_house_price(house_id, new_price, engine)
+
+    def test_API_should_check_if_house_id_to_be_reserved_already_exists_in_the_database(self, setup_database):
+        DBFILE, engine = setup_database
+        house_id = 100 #house id doesn't exist
+        new_price = 1000
+        with pytest.raises(ValueError, match="House id doesn't exist"):
+            HousingAPI.reserve_house(house_id, engine)
+
+    def test_API_should_check_if_house_id_to_be_removed_exists_in_the_database(self, setup_database):
+        DBFILE, engine = setup_database
+        house_id = 100 #house id doesn't exist
+
+        with pytest.raises(ValueError, match="House id doesn't exist"):
+            HousingAPI.remove_house(house_id, engine)
+            
+    def test_house_id_to_be_price_checked_must_exist_in_the_database(self, setup_database):
+        """checks if house id to be price checked exists"""
+        DBFILE, engine = setup_database
+        house_id = 100 #house id doesn't exist
+        new_price = 1000
+        with pytest.raises(ValueError, match="House id doesn't exist"):
+            HousingAPI.check_house_price(house_id, engine)
+
 
     def test_API_should_properly_update_reservered_houses(self, setup_database):
         DBFILE, engine = setup_database
@@ -128,6 +157,23 @@ class TestAPIOfHousing:
         DBFILE, engine = setup_database
         with pytest.raises(ValueError, match="No matching house id"):
             HousingAPI.sell_house(100,"cash", engine_object=engine)
+            
+    #@pytest.mark.skip(reason="no way of currently testing this")
+    def test_sell_house_should_raise_error_if_commission_percent_is_more_than_100_or_less_than_0(self, setup_database):
+        DBFILE, engine_object = setup_database
+        house_id = 2
+        financing_option = "cash"
+        commission_percent_over_100 = 100.1
+        negative_commission_percent = -0.1
+        
+        with pytest.raises(ValueError, match= "Commission percent should be between 0 and 100"):
+
+            HousingAPI.sell_house(house_id, financing_option, commission_percent_input=commission_percent_over_100, engine_object=engine_object)
+        
+        with pytest.raises(ValueError, match= "Commission percent should be between 0 and 100"):
+
+            HousingAPI.sell_house(house_id, financing_option, commission_percent_input=negative_commission_percent, engine_object=engine_object)
+
 
 
     def test_API_should_check_if_payment_value_is_more_than_loan(self):
